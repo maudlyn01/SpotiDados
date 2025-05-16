099aa7c3c6609d7fc6dc2902d63af77d1fa61d5d
geniamaria <mariageniaabilio@gmail.com>
2025-05-16 12:31:28 +0200

    fixed: router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Routes } from './router'
-'./router.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Routes } from "react-router-dom";
 
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    < Routes />
-  </StrictMode>,
-)
+    <Routes />
+  </StrictMode>
+);
