76d0f53999c58c465dedf5bc785e09f12a20b961
Mchilzia <chilziamacamo011@gmail.com>
2025-05-16 14:17:58 +0200

    feat login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,20 @@
 export const Login = () => {
     return(
         <div>
-            
+             <div className="input-button ">
+                
+                <input type="username" placeholder="username" />
+                </div>
+                <div>
+                <input type="email"  placeholder="email"/>
+                </div>
+                <div>
+                <input type="password" placeholder="password"/>
+            </div>
+            <div>
+            <button className="" type="submit">sig in</button>
+            </div>
+
         </div>
     )
 }
