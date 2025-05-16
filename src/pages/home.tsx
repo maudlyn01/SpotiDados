d2b0855e38b910f83f38001bf12abea4326289ec
geniamaria <mariageniaabilio@gmail.com>
2025-05-16 15:35:05 +0200

    falha

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,13 @@
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
-
 export const Home = () => {
   return (
     <>
-      <main className="bg-black  min-h-screen">
+      <div className="bg-black min-h-screen">
         <Header />
-        <p className="text-white"> ola</p>
+        <main className="  flex-1"></main>
         <Footer />
-      </main>
+      </div>
     </>
   );
 };
