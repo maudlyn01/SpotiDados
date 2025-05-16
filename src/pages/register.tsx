0d4ef411178567bb4f5adc1421608166c7a47d92
Gabriel-webmaster <gabrielalfredocuna9@gmail.com>
2025-05-16 16:35:51 +0200

    update acount create

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -7,7 +7,7 @@ export const Register = () => {
         <div className="bg-black">
             <Header />
             <h2 style={{position:"relative", top:"189px", left:"150px"}}>Create new acount</h2>
-            < input type="name" placeholder="name" style={{position:"relative", top:"1px", left:"350px", backgroundColor:"white", borderRadius:"15px"}} />
+            < input type="name" placeholder="name" style={{position:"relative", top:"1px", left:"350px", backgroundColor:"blue", borderRadius:"15px"}} />
             <input type="surname" placeholder="surname" style={{position:"relative", top:"50px", left:"170px" , backgroundColor:"white", borderRadius:"15px"}} />
             <input type="username" placeholder="username" style={{position:"relative", top:"112px", left:"px", backgroundColor:"brown", borderRadius:"15px" }}/>
             <input type="password" placeholder="Password" style={{position:"relative", top:"160px", right:"180px", backgroundColor:"brown", borderRadius:"15px"}}/>
