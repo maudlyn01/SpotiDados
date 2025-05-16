53c92fadd74e17dfb87a15f6ac90d2245f5a8af1
geniamaria <mariageniaabilio@gmail.com>
2025-05-16 13:32:32 +0200

    fixed: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,16 @@
+import { House, Book, MagnifyingGlass } from "phosphor-react";
+
 export const Footer = () => {
   return (
     <>
       <footer>
-        <nav className="text-amber-300 fixed"> home</nav>
+        <nav className="text-amber-300 ">
+          <div className="flex justify-around items-baseline-last">
+            <Book size={32} />
+            <House size={32} weight="fill" />
+            <MagnifyingGlass size={32} />
+          </div>
+        </nav>
       </footer>
     </>
   );
