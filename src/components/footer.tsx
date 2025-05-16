e84674fd033bafae36df762d4cab01fc6e0109ea
Gabriel-webmaster <gabrielalfredocuna9@gmail.com>
2025-05-16 14:31:55 +0200

    update login

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 export const Footer = () => {
     return(
         <footer>
+        
             
         </footer>
     )
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,19 @@
+import { Placeholder } from "phosphor-react"
+
 export const Register = () => {
     return(
         <div>
-            
+            <h2>Create new acount</h2>
+            < input type="name" placeholder="name" />
+            <input type="surname" placeholder="surname"/>
+            <input type="username" placeholder="username"/>
+            <input type="password" placeholder="Password"/>
+            <input type="confirm password" placeholder="password"/>
+            <img src="" alt="" />
+
+            <button style={{width:"20px", height:"7px", borderRadius:"6px" }}>Registar</button>
+            <p></p>
+
         </div>
     )
 }
