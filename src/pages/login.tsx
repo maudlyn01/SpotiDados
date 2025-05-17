300e78863258bbcaf7411737321d4d823bd975d3
Maria Genia Abilio Moiane <mariageniaabilio@gmail.com>
2025-05-17 19:53:40 +0200

    fix: page Login in this can not have header and footer,:

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,30 +1,22 @@
-import { Footer } from "../components/footer"
-import { Header } from "../components/header"
-
 export const Login = () => {
     return(
 
         <div className="bg-black">
-            <Header />
+         
             <main>
-              
-        <div>
-             <div className="input-button ">
-                
-                <input type="username" placeholder="username" />
+                <div className="input-button ">
+                    <input type="username" placeholder="username" />
                 </div>
                 <div>
-                <input type="email"  placeholder="email"/>
+                    <input type="email"  placeholder="email"/>
                 </div>
                 <div>
-                <input type="password" placeholder="password"/>
-            </div>
-            <div>
-            <button className="" type="submit">sig in</button>
-            </div>
-        </main>
-            <Footer />
-
+                    <input type="password" placeholder="password"/>
+                </div>
+                <div>
+                    <button className="" type="submit">sig in</button>
+                </div>
+            </main>
         </div>
     )
 }
