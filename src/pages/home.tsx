fa9332ad0ce936953c17270397392aac8e85a58d
LucoVilanculos <angelatave8@gmail.com>
2025-05-19 09:41:34 +0200

    feat button function on router

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
-import { StatusBar } from "../components/networkTimeBar";
 
 export const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="relative min-h-screen bg-black text-white">
-      <StatusBar />
+    <div className="bg-black p-16 min-h-screen flex">
+
       <Header />
-      <main className="flex-1">
-        {/* coloque aqui o codigo de home */}
+      <main className="p-9 flex justify-end gap 4">
+        <button className="btn" onClick={() => navigate("/login")}>login</button> <button className="btn" onClick={() => navigate("/register")}>sing-in</button>
+        
       </main>
       <Footer />
     </div>
