6fbbe18e169cd552b2bac908dc9daf0b778f7124
maudlyn01 <maudana2000@gmail.com>
2025-05-19 15:13:24 +0200

    clean tree

--- src/pages/splashingScreen.tsx
+++ src/pages/splashingScreen.tsx
@@ -1,9 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
 
-=======
->>>>>>> 22cd1869760a031fefe2a0ed301f5695a1f3ab5b
 
 export const SplashingScreen = () => {
   const navigate = useNavigate();
@@ -18,10 +15,7 @@ export const SplashingScreen = () => {
   return (
     <>
       <div className="flex items-center justify-center h-screen bg-black relative">
-<<<<<<< HEAD
 
-=======
->>>>>>> 22cd1869760a031fefe2a0ed301f5695a1f3ab5b
         {/* Logo*/}
         <div className="flex flex-col items-center">
           <img src="/logo.png" alt="spotidados logo" className="w-64 h-auto" />
