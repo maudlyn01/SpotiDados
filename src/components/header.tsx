039cd1fd06633e860bc36c26fe9b134295381829
maudlyn01 <maudana2000@gmail.com>
2025-05-21 05:59:21 +0200

    feat: only show logout when the user is loged

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { List, X } from "phosphor-react";
+
+const isLoggedIn = true;
 
 export const Header = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const { pathname } = useLocation();
   const modal = () => setModalOpen(!modalOpen);
 
   return (
 
 
-    
+
     <header className="w-full px-4 py-2 bg-black text-sm text-white shadow-md mb-0 ">
       <div className="flex justify-between items-center">
         <img src="/img/Spoti.png" alt="" className="w-20 h-20 object-contain" />
@@ -23,22 +26,23 @@ export const Header = () => {
         </button>
       </div>
       {modalOpen && (
-        
+
         <nav className="flex flex-col items-center space-y-3 mt-4 md:hidden text-center">
-          <NavLink to="/" className="burguerMenu" onClick={modal}>
+          {/*<NavLink to="/" className="burguerMenu" onClick={modal}>
             Start
-          </NavLink>
+          </NavLink>*/}
           <NavLink to="/user" className="burguerMenu" onClick={modal}>
             My Profile
           </NavLink>
           <NavLink to="/HomeUser" className="burguerMenu" onClick={modal}>
-
             Home
           </NavLink>
+           {isLoggedIn && pathname !== "/home" && (
           <NavLink to="/home" className="burguerMenu" onClick={modal}>
             Log Out
           </NavLink>
-          
+           )}
+
         </nav>
       )}
     </header>
